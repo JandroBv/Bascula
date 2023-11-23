89fab58a70e5433041a0f4565c3085dff1d1d43c
Jesús Alejandro Murillo Quintero <jesusmurillo28052@gmail.com>
2023-11-23 14:35:59 -0700

    PaginaBasculaInicial

--- Bascula/pages/PaginaBasculaInicial.xaml.cs
+++ Bascula/pages/PaginaBasculaInicial.xaml.cs
@@ -23,10 +23,5 @@ namespace Bascula.pages
         {
             InitializeComponent();
         }
-
-        private void Button_Click(object sender, RoutedEventArgs e)
-        {
-            
-        }
     }
 }
